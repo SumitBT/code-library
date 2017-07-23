3638ee6ab4c825c7150f4d06bdc5102c6b22f0de
Sumit Tamgale <sumittamgale@gmail.com>
2017-07-23 07:41:38 +0530

    get public ready

--- codeathon/round1/q10.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main()
-{
-  printf("Enter the object you want to find perimeter for:1 for circle,
-  return 0;
-}
--- codevita-2016/cvpb.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include<cmath>
-using namespace std;
-int main(void)
-{
-    int n,i;
-    cin>>n>>i;
-    return 0;
-}
